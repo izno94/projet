ec2ba7f6284ddb2a57c2591bf1baf769853961d4
Romain <mess@gmail.com>
2019-10-08 23:51:43 +0200

    Auto-generated commit

--- src/app/more.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MorePipe } from './more.pipe';
-
-describe('MorePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MorePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
